f3ceb64de0c8e4755fe48b9a09e17aafbe2b6b43
DanilaTalalay <32534496+DanilaTalalay@users.noreply.github.com>
2022-10-19 20:52:23 +0300

    Add files via upload

---
+++ Language_Java_Basics_1/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+
+        String fullName = ("Talalay Danila Andreevich");
+        String birthDate = ("1997.12.09");
+        String placeOfResidence = ("Korenovsk");
+        int age = 24;
+        String profeshion = "Informathion security";
+        System.out.println(fullName);
+        System.out.println(birthDate);
+        System.out.println(placeOfResidence);
+        System.out.println(age);
+        System.out.println(profeshion);
+    }
+
+}
